51f5388d95eb6dd1f36adc5951ce8564892f2580
Sairaj Vakalapuri <138584331+Sairaj2212@users.noreply.github.com>
2024-02-03 12:00:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,18 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  let count = localStorage.getItem("viewCount");
+
+  if (count === null) {
+      count = 0;
+  } else {
+      count = parseInt(count);
+  }
+
+  count++;
+
+  localStorage.setItem("viewCount", count);
+  document.getElementById("viewCount").textContent = "Page Views: " + count;
+});
