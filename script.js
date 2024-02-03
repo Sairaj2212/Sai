0221423779acc5083d2c08ed28221d46aa77f4cc
Sairaj Vakalapuri <138584331+Sairaj2212@users.noreply.github.com>
2024-02-03 12:12:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,21 @@ function toggleMenu() {
   icon.classList.toggle("open");
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  let count = localStorage.getItem("viewCount");
-
-  if (count === null) {
-      count = 0;
-  } else {
-      count = parseInt(count);
-  }
-
-  count++;
-
-  localStorage.setItem("viewCount", count);
-  document.getElementById("viewCount").textContent = "Page Views: " + count;
-});
+expiration = new Date;
+expiration.setMonth(expiration.getMonth()+6)
+counter = eval(cookieVal("total_visited"))
+counter++
+document.cookie = "total_visited="+counter+";expires=" + expiration.toGMTString()
+ 
+ 
+function cookieVal(cookieName) {
+	thisCookie = document.cookie.split("; ")
+	for (i=0; i<thisCookie.length; i++){
+		if (cookieName == thisCookie[i].split("=")[0]){
+			return thisCookie[i].split("=")[1]
+		}
+	}
+	return 0;
+}
+ 
+document.getElementById('result').innerHTML = "<center><h3>You visited this page <label style='font-size:40px;' class='text-info'>"+counter+"</label> times.</h3></center>";
